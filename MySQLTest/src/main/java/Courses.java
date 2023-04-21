2152c227d45b466d877c6dc1c37ad36ad8d65127
dXdyZ <115111263+dXdyZ@users.noreply.github.com>
2023-04-21 23:17:58 +0300

    Add files via upload

---
+++ MySQLTest/src/main/java/Courses.java
@@ -0,0 +1,101 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Courses")
+public class Courses {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+    private  String name;
+    private int duration;
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "enum")
+    private CourseType type;
+    private  String description;
+    @Column(name = "teacher_id")
+    private int teacherID;
+    @Column(name = "student_count")
+    private int studentsCount;
+    private  int price;
+    @Column(name = "price_per_hour")
+    private float pricePerHour;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public CourseType getType() {
+        return type;
+    }
+
+    public void setType(CourseType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(int teacherID) {
+        this.teacherID = teacherID;
+    }
+
+    public int getStudentsCount() {
+        return studentsCount;
+    }
+
+    public void setStudentsCount(int studentsCount) {
+        this.studentsCount = studentsCount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public float getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(float pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+
+
+
+
+
+
+}
---
+++ MySQLTest/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        SessionFactory session = sessionFactory.openSession();
+
+        Courses courses = session.get
+
+        sessionFactory.close();
+
+
+
+    }
+}
