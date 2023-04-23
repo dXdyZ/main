d8d1a49c31a9903fa72f17290dff2185ed5b33b3
dXdyz <ttttilinn85@gmail.com>
2023-04-23 18:17:29 +0200

    metod

--- bookLibrary/src/main/java/com/BookLibrary/bookLibrary/BookController.java
+++ bookLibrary/src/main/java/com/BookLibrary/bookLibrary/BookController.java
@@ -1,21 +1,32 @@
 package com.BookLibrary.bookLibrary;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import response.Book;
 
 import java.util.List;
 
 @RestController
 public class BookController {
-    @RequestMapping(value = "/books/", method = RequestMethod.GET)
+    @GetMapping("/books/")
     public List<Book> list(){
         return Storage.getAllBooks();
     }
 
-    @RequestMapping(value = "/books/", method = RequestMethod.POST)
+    @PostMapping("/books/")
     public int add(Book book){
         return Storage.addBook(book);
+    }
+
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity get(@PathVariable int id){
+        Book book = Storage.getBook(id);
+        if(book == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return  new ResponseEntity(book, HttpStatus.OK);
     }
 }
