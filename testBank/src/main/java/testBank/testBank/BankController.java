f6337fd212bec2a979e97f246b2add235253e34b
dXdyz <ttttilinn85@gmail.com>
2023-04-25 00:40:14 +0200

    t

---
+++ testBank/src/main/java/testBank/testBank/BankController.java
@@ -0,0 +1,10 @@
+package testBank.testBank;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Controller
+public class BankController {
+}
