aa22ece246e94196622be7b41b72a1d902ac25cc
dXdyz <ttttilinn85@gmail.com>
2023-04-27 22:33:59 +0200

    g

--- Obuchenie/src/Main.java
+++ Obuchenie/src/Main.java
@@ -1,21 +1,5 @@
-
 public class Main {
     public static void main(String[] args) {
-    }
-}
-class Human{
-    private String name;
-    private int age;
 
-    public Human(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 }
