e1981c47ed63277c50f2b854904946592abda780
dXdyz <ttttilinn85@gmail.com>
2023-04-29 18:11:42 +0200

    y

--- Obuchenie/src/Main.java
+++ Obuchenie/src/Main.java
@@ -1,14 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        readFile();
-    }
+    public static void main(String[] args) {
 
-    public static void  readFile() throws FileNotFoundException {
-        File file = new File("test.txt");
-        Scanner scanner = new Scanner(file);
     }
 }
