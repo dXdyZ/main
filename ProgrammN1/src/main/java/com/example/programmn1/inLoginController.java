2d41be398a16ec0ebf56f156fbcf6e130a90de8b
ttttilinn85@gmail.com <ttttilinn85@gmail.com>
2023-06-05 23:42:50 +0200

    ProgrammN1/inLogin new

--- ProgrammN1/src/main/java/com/example/programmn1/inLoginController.java
+++ ProgrammN1/src/main/java/com/example/programmn1/inLoginController.java
@@ -1,8 +1,14 @@
 package com.example.programmn1;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class inLoginController {
 
@@ -13,8 +19,29 @@ public class inLoginController {
     private URL location;
 
     @FXML
+    private Button unloginButton;
+
+    @FXML
     void initialize() {
+        unloginButton.setOnAction(actionEvent -> {
+            unloginButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("hello-view.fxml"));
+
+            try {
+                loader.load();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            Parent rootUn = loader.getRoot();
+            Stage stageUn = new Stage();
+            stageUn.setScene(new Scene(rootUn));
+            stageUn.showAndWait();
+        });
 
     }
-
 }
+
+
